50496a1218ef4c19415b1102de8c732566c0c29f
Toby Lopez <sudo.user.lopez@outlook.com>
2025-10-27 16:52:35 +0000

    lol

--- 0.1.0/main.mod.js
+++ 0.1.0/main.mod.js
@@ -1,8 +1,8 @@
 import { PolyMod } from "https://cdn.polymodloader.com/PolyTrackMods/PolyModLoader/0.5.1/PolyModLoader.js";
-class 0909 extends PolyMod {
+class ninethousandandninety extends PolyMod {
     
 }
 
 
-export let polyMod = new 0909();
+export let polyMod = new ninethousandandninety();
 
