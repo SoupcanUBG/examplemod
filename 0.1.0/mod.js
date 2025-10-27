e5dd986141a5bfc5c96d68f1bbb4421c41fabb8c
Toby Lopez <sudo.user.lopez@outlook.com>
2025-10-27 16:13:32 +0000

    origin

---
+++ 0.1.0/mod.js
@@ -0,0 +1,17 @@
+import { PolyMod } from "https://cdn.polymodloader.com/PolyTrackMods/PolyModLoader/0.5.1/PolyModLoader.js";
+class Proxy extends PolyMod {
+   init = (pml) => {
+    this.pml = pml;
+
+   } 
+  postInit = () => {
+
+  }
+  simInit = () => {
+
+  }
+}
+
+
+export let polyMod = new YourModClass();
+
